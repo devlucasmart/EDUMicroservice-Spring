43fb0ccbd6b47a2cacf3414da1eaa3b44d9081f4
Lucas Martins <lucas.martins.fsa@gmail.com>
2023-07-17 22:01:23 -0300

    estruturação inicial

---
+++ src/main/java/com/devlucasmart/EduMicro/EduMicroApplication.java
@@ -0,0 +1,13 @@
+package com.devlucasmart.EduMicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduMicroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devlucasmart/EduMicro/EduMicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlucasmart.EduMicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduMicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
